396f9df8478f648d8bba871ec96f2d69c52cdde7
Farouq Seriki <farouq@basaltx.io>
2024-08-03 01:12:39 +0100

    Route Protection - auth gatekeeping

---
+++ app/[home]/index.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+import Map from 'components/Map';
+import { StatusBar } from 'expo-status-bar';
+import SelectedScooterSheet from '~/components/SelectedScooterSheet';
+
+export default function Home() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Home', headerShown: false }} />
+      <Map />
+      <SelectedScooterSheet />
+      <StatusBar style="light" />
+    </>
+  );
+}
---
+++ providers/AuthProvider.tsx
@@ -0,0 +1,45 @@
+import { Session } from '@supabase/supabase-js';
+import { createContext, PropsWithChildren, useContext, useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { supabase } from '~/lib/supabase';
+interface authContexttype {
+  userSession?: Session | null;
+  isAuthenticated: boolean;
+}
+
+const AuthContext = createContext<authContexttype | null>({
+  isAuthenticated: false,
+});
+
+export default function AuthProvider({ children }: PropsWithChildren) {
+  const [userSession, setUserSession] = useState<Session | null>();
+  const [isReady, setIsReady] = useState<boolean>(false);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setUserSession(session);
+      setIsReady(true);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setUserSession(session);
+    });
+  }, []);
+
+  if (!isReady) {
+    return <ActivityIndicator />;
+  }
+
+  return (
+    <AuthContext.Provider value={{ userSession, isAuthenticated: !!userSession?.user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+  return context;
+};
